65b681bc0a772a13275f4fcb656da1270bf90bd6
maddy <maddy-dolly>
2020-02-23 04:37:19 +0530

    kya karu

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                                                <router-link :to="{name:'user'}">Go to Home</router-link>
+
+                    </div>
+                </div>
+            </div>
+        </div><div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                       <router-view></router-view>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/js/components/User.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+           <router-link :to="{name:'home'}">Go to Home</router-link>
+              hello 
+                  
+            </div>
+            <div class="col-md-4">
+  <router-view></router-view>
+        </div>
+        </div>
+        
+                  
+
+    </div>
+</template>
+  
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
